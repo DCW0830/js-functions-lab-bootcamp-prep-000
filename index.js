70fdb8356b8e00b7ddc05c2dc6c009c13be4c563
Daniel Walz <walzdan@gmail.com>
2018-02-19 01:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function holidayCountdown(holiday, days)
   return(`It's ${days} days until ${holiday}!`)
 }
 MerryChristmasTo("Daniel")
-happyHolidayTo("Independence, Daniel")
-holidayCountdown("Mother's day",20)
+happyHolidayTo("Independence Day, Daniel")
+holidayCountdown("Mother's Day",20)
